3ae1f6d487894b087ff5cde623d94dc83c948806
shyamsha <shyamkumar97@live.com>
2021-08-17 17:59:56 +0530

    integrate onedrive storage

--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ import useDrivePicker from "react-google-drive-picker";
 function App() {
   const [setFileSelected, setSetFileSelected] = useState();
   const [openPicker, data] = useDrivePicker();
+
   useEffect(() => {
     if (data) {
       setSetFileSelected(data.docs[0]);
     }
   }, [data]);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://js.live.net/v7.2/OneDrive.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   const googleLogin = (response) => {
     console.log(response);
@@ -21,8 +32,6 @@ function App() {
   const fileReader = (e) => {
     setSetFileSelected(e.target.files[0]);
   };
-
-  const oneDrive = () => {};
 
   const dropBox = (file) => {
     // let dropBox = document.createElement("script");
@@ -33,42 +42,42 @@ function App() {
     // dropBox.setAttribute("id", "dropboxjs");
     // dropBox.setAttribute("data-app-key", "<YOU_ API_KEY>");
     // document.head.appendChild(dropBox);
-    const options = {
-      // Required. Called when a user selects an item in the Chooser.
-      success: function (files) {
-        alert("Here's the file link: " + files[0].link);
-      },
+    // const options = {
+    //   // Required. Called when a user selects an item in the Chooser.
+    //   success: function (files) {
+    //     alert("Here's the file link: " + files[0].link);
+    //   },
 
-      // Optional. Called when the user closes the dialog without selecting a file
-      // and does not include any parameters.
-      cancel: function () {},
+    //   // Optional. Called when the user closes the dialog without selecting a file
+    //   // and does not include any parameters.
+    //   cancel: function () {},
 
-      // Optional. "preview" (default) is a preview link to the document for sharing,
-      // "direct" is an expiring link to download the contents of the file. For more
-      // information about link types, see Link types below.
-      linkType: "preview", // or "direct"
+    //   // Optional. "preview" (default) is a preview link to the document for sharing,
+    //   // "direct" is an expiring link to download the contents of the file. For more
+    //   // information about link types, see Link types below.
+    //   linkType: "preview", // or "direct"
 
-      // Optional. A value of false (default) limits selection to a single file, while
-      // true enables multiple file selection.
-      multiselect: false, // or true
+    //   // Optional. A value of false (default) limits selection to a single file, while
+    //   // true enables multiple file selection.
+    //   multiselect: false, // or true
 
-      // Optional. This is a list of file extensions. If specified, the user will
-      // only be able to select files with these extensions. You may also specify
-      // file types, such as "video" or "images" in the list. For more information,
-      // see File types below. By default, all extensions are allowed.
-      extensions: [".pdf", ".doc", ".docx", ".txt"],
+    //   // Optional. This is a list of file extensions. If specified, the user will
+    //   // only be able to select files with these extensions. You may also specify
+    //   // file types, such as "video" or "images" in the list. For more information,
+    //   // see File types below. By default, all extensions are allowed.
+    //   extensions: [".pdf", ".doc", ".docx", ".txt"],
 
-      // Optional. A value of false (default) limits selection to files,
-      // while true allows the user to select both folders and files.
-      // You cannot specify `linkType: "direct"` when using `folderselect: true`.
-      folderselect: false, // or true
+    //   // Optional. A value of false (default) limits selection to files,
+    //   // while true allows the user to select both folders and files.
+    //   // You cannot specify `linkType: "direct"` when using `folderselect: true`.
+    //   folderselect: false, // or true
 
-      // Optional. A limit on the size of each file that may be selected, in bytes.
-      // If specified, the user will only be able to select files with size
-      // less than or equal to this limit.
-      // For the purposes of this option, folders have size zero.
-      sizeLimit: 1024, // or any positive number
-    };
+    //   // Optional. A limit on the size of each file that may be selected, in bytes.
+    //   // If specified, the user will only be able to select files with size
+    //   // less than or equal to this limit.
+    //   // For the purposes of this option, folders have size zero.
+    //   sizeLimit: 1024, // or any positive number
+    // };
     // Dropbox.choose(options);
     setSetFileSelected(file[0]);
   };
@@ -82,6 +91,49 @@ function App() {
       // token: token, // pass oauth token in case you already have one
       supportDrives: true,
     });
+  };
+
+  const launchOneDrivePicker = () => {
+    return new Promise((resolve, reject) => {
+      var odOptions = {
+        clientId: "7ea71fb9-e8a0-4baa-9ac9-be91a7e80783",
+        action: "query",
+        multiSelect: false,
+        openInNewWindow: true,
+        advanced: {
+          filter: "folder,.pdf",
+          //filter: "folder,photo"
+        },
+        success: (files) => {
+          resolve(files);
+        },
+        cancel: () => {
+          resolve(null);
+        },
+        error: (e) => {
+          reject(e);
+        },
+      };
+      window.OneDrive.open(odOptions);
+    });
+  };
+
+  const oneDrive = (e) => {
+    e.preventDefault();
+    launchOneDrivePicker()
+      .then((result) => {
+        if (result) {
+          console.log(result.value[0]);
+          // for (const file of result.value) {
+          //   const name = file.name;
+          //   const url = file["@microsoft.graph.downloadUrl"];
+          //   console.log({ name: name, url: url });
+          // }
+        }
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
   };
 
   return (
@@ -119,7 +171,7 @@ function App() {
         <div>
           <p></p>
         </div>
-        <div onClick={oneDrive} style={{ cursor: "pointer" }}>
+        <div onClick={oneDrive} id="OpenOneDrive" style={{ cursor: "pointer" }}>
           oneDrive
         </div>
         <div>
@@ -132,7 +184,7 @@ function App() {
           multiselect={false}
           extensions={[".pdf"]}
         >
-          <div onClick={() => null} style={{ cursor: "pointer" }}>
+          <div onClick={dropBox} style={{ cursor: "pointer" }}>
             dropBox
           </div>
         </DropboxChooser>
