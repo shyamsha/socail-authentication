276e34c9ec366e294246459409edc6c0007c5d3d
shyamsha <shyamkumar97@live.com>
2021-08-26 20:21:08 +0530

    remove unwanted code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { GoogleLogin } from "react-google-login";
-import DropboxChooser from "react-dropbox-chooser";
 import useDrivePicker from "react-google-drive-picker";
 
 function App() {
@@ -11,6 +10,13 @@ function App() {
   useEffect(() => {
     if (data) {
       setSetFileSelected(data.docs[0]);
+      console.log(data);
+      fetch(`https://www.googleapis.com/drive/v3/files/${setFileSelected.id}`)
+        .then((res) => {
+          console.log(res);
+          res.json();
+        })
+        .then((result) => console.log(result));
     }
   }, [data]);
 
@@ -24,17 +30,17 @@ function App() {
     };
   }, []);
 
-  // useEffect(() => {
-  //   let dropBox = document.createElement("script");
-  //   dropBox.src = "https://www.dropbox.com/static/api/2/dropins.js";
-  //   dropBox.async = true;
-  //   dropBox.setAttribute("id", "dropboxjs");
-  //   dropBox.setAttribute("data-app-key", "fz7kjqq7ofzgi60");
-  //   document.body.appendChild(dropBox);
-  //   return () => {
-  //     document.body.removeChild(dropBox);
-  //   };
-  // }, []);
+  useEffect(() => {
+    let dropBox = document.createElement("script");
+    dropBox.src = "https://www.dropbox.com/static/api/2/dropins.js";
+    dropBox.async = true;
+    dropBox.setAttribute("id", "dropboxjs");
+    dropBox.setAttribute("data-app-key", "fz7kjqq7ofzgi60");
+    document.body.appendChild(dropBox);
+    return () => {
+      document.body.removeChild(dropBox);
+    };
+  }, []);
 
   useEffect(() => {
     let linkedinAPi = `https://api.linkedin.com/v2/me`;
@@ -71,18 +77,19 @@ function App() {
   };
 
   const dropBox = (file) => {
-    // const options = {
-    //   success: function (files) {
-    //     alert("Here's the file link: " + files[0].link);
-    //   },
-    //   cancel: function () {},
-    //   linkType: "preview",
-    //   multiselect: false,
-    //   extensions: [".pdf"],
-    //   folderselect: false,
-    //   // sizeLimit: 1024,
-    // };
-    // window.Dropbox.choose(options);
+    const options = {
+      success: function (files) {
+        alert("Here's the file link: " + files[0].link);
+        console.log(files);
+      },
+      cancel: function () {},
+      linkType: "preview",
+      multiselect: false,
+      extensions: [".pdf"],
+      folderselect: false,
+      // sizeLimit: 1024,
+    };
+    window.Dropbox.choose(options);
     setSetFileSelected(file[0]);
   };
 
@@ -101,7 +108,7 @@ function App() {
     return new Promise((resolve, reject) => {
       var odOptions = {
         clientId: "7ea71fb9-e8a0-4baa-9ac9-be91a7e80783",
-        action: "query",
+        action: "download",
         multiSelect: false,
         openInNewWindow: true,
         advanced: {
@@ -149,21 +156,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <LinkedIn
-          clientId="781sbw7lgx37yi"
-          onSuccess={linkedin}
-          onFailure={handleFailure}
-          redirectUri="http://localhost:3000"
-          renderElement={({ onClick, disabled }) => (
-            <div
-              onClick={onClick}
-              disabled={disabled}
-              style={{ cursor: "pointer" }}
-            >
-              Login with Linkedin
-            </div>
-          )}
-        /> */}
         <div onClick={linkedin} style={{ cursor: "pointer" }}>
           Login with Linkedin
         </div>
@@ -171,30 +163,12 @@ function App() {
           <p></p>
         </div>
         <GoogleLogin
-          clientId="970824446389-c8aog99f5ui467iq4lf40gm49ec1jpsk.apps.googleusercontent.com"
+          clientId="970824446389-jdbnim2aj2df7krahcflm8ifhk4s1tlk.apps.googleusercontent.com"
           buttonText="Login with Google"
           onSuccess={googleLogin}
           onFailure={googleLogin}
           cookiePolicy={"single_host_origin"}
         />
-        {/* <div
-          id="g_id_onload"
-          data-client_id="869678648838-3a216u28qn05q2qo7c8f7rqq6uhahctr.apps.googleusercontent.com"
-          // data-login_uri="www.google.com"
-          data-context="signup"
-          data-ux_mode="popup"
-          data-callback="signup"
-          data-auto_prompt="false"
-        ></div>
-        <div
-          class="g_id_signin"
-          data-type="standard"
-          data-shape="pill"
-          data-theme="outline"
-          data-text="signin_with"
-          data-size="large"
-          data-logo_alignment="left"
-        ></div> */}
         <div>
           <p></p>
         </div>
@@ -208,17 +182,9 @@ function App() {
         <div>
           <p></p>
         </div>
-        <DropboxChooser
-          appKey={"fz7kjqq7ofzgi60"}
-          success={(file) => dropBox(file)}
-          cancel={() => console.log("closed")}
-          multiselect={false}
-          extensions={[".pdf"]}
-        >
-          <div onClick={dropBox} style={{ cursor: "pointer" }}>
-            dropBox
-          </div>
-        </DropboxChooser>
+        <div onClick={dropBox} style={{ cursor: "pointer" }}>
+          dropBox
+        </div>
         <div>
           <p></p>
         </div>
